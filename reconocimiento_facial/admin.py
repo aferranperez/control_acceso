5e0b8e798597e886ebf65bad8fec2118f5d6e700
Alejandro Ferran <aferranperez@gmail.com>
2021-09-02 21:09:39 -0600

    Crea fichero para dataset, (falta subir imagenes a localizacion)

--- reconocimiento_facial/admin.py
+++ reconocimiento_facial/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.db import models
+from shutil import *
+import os
 
 # Register your models here.
 from .models import *
+
+#Registre acciones aqui
+@admin.action(description="Mover imagenes subidas al Dataset")
+def mover_images_dataset(modeladmin, request, queryset):
+    ficheros = os.listdir("tmp/")
+
+    for obj in queryset:
+        for fichero in ficheros:
+            dir = "tmp" + "/" + fichero
+            dir_dest = str("media/reconocimiento_facial/" + obj.nombre + obj.apellido + "/")
+            move(dir, dir_dest)    
+
+
+
 
 
 #Clase para administrar el modelo Persona 
@@ -12,11 +28,11 @@ class PersonaModelAdmin(admin.ModelAdmin):
     search_fields = ["nombre", "apellido"]
     ordering = ["nombre"]
     inlines = [InlineImage]
+    actions = [mover_images_dataset]
+   
     #Especifica el modelo del cual se van a sacar todos los valores anteriores
     class Meta:
         model = Persona
 
-
 admin.site.register(Persona, PersonaModelAdmin)
-
 
--- reconocimiento_facial/models.py
+++ reconocimiento_facial/models.py
@@ -1,6 +1,8 @@
+from abc import abstractclassmethod
 from django.db import models
 from django.contrib import admin
 from django.db.models.deletion import CASCADE
+
 
 # Create your models here.
 
@@ -13,12 +15,24 @@ class Persona(models.Model):
     CI = models.CharField(max_length=20)
     edad = models.IntegerField()
 
+    #Sobrescribimos el metodo save para que cree la carpeta del dataset de cada persona
+    def save(self):
+        import os
+        folder = 'media/reconocimiento_facial/' + self.nombre +' '+ self.apellido
+        
+        if not os.path.exists(folder):
+            os.mkdir(str(folder))
+        
+        super(Persona, self).save()
+
     def __str__(self):
         return self.nombre
-    
+
+
 class Imagen(models.Model):
     persona = models.ForeignKey(Persona, on_delete=CASCADE)
-    image = models.ImageField(upload_to='reconocimiento_facial/')
+    image = models.ImageField(upload_to='tmp/')
+
 
 class InlineImage(admin.TabularInline):
     model = Imagen
